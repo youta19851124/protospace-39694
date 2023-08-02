a475f79eea59aa25258071825251ebfb3667f2f0
youta19851124 <youta19851124@gmail.com>
2023-08-02 18:11:56 +0900

    initial commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,59 @@
+class PrototypesController < ApplicationController
+  before_action :set_prototype, only: [:edit, :show, :update]
+  before_action :move_to_index, except: [:index, :show]
+
+  def index
+    @prototypes = Prototype.includes(:user).order("created_at DESC")
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  def edit
+  end
+
+  def update
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+end
---
+++ app/models/prototype.rb
@@ -0,0 +1,14 @@
+class Prototype < ApplicationRecord
+
+  belongs_to :user
+  has_many :comments
+  has_one_attached :image
+
+  validates :title, presence: true
+  validates :catch_copy, presence: true
+  validates :concept, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
+end
